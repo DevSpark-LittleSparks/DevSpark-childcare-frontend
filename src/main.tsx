19be214f7e03d4126057efebd7cb9e089b2f62d5
Senuri Wickramasinghe <wickramasinghewarangana@gmail.com>
2026-04-23 22:37:08 +0530

    fix: resolved path error in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App'; // Path corrected to current folder
+import { Provider } from 'react-redux';
+
+// Paths checked based on your sidebar
+import App from './App'; 
+import { store } from './store'; 
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+}
